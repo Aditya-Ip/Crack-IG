2d0a9e2c23d1cf63c85c645312c475e09beac12e
Aditya-Ip <126338852+Aditya-Ip@users.noreply.github.com>
2023-02-25 21:53:36 +0700

    Add files via upload

---
+++ xnxx.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python2
+# coding=utf-8
+import os,sys,time,itertools,datetime,random,re,threading,json,getpass,urllib
+from multiprocessing.pool import ThreadPool
+vindragamingslewebjancokmenabnjaylelebwelehphqvindraanjaygialaselebejeksie = "haoaoapajLHalalabObakaoabOabalLMabalabOqbKqbalNalan9pabaoHaksohapsbwhakqlwsg719pahajaiagapqKLHIFOGIKa3T40"
+import mechanize
+import requests
+import hashlib
+import fake_useragent
+
+:os.system('git pull')
+
+
+time.sleep(4.3)
+
+
+
+
+os.system('clear')
+
+print ("\033[1;96m $$$$$$$\   $$$$$$\  $$$$$$$\  $$\   $$\  $$$$$$\  ")
+print ("\033[1;96m $$  __$$\ $$  __$$\ $$  __$$\ $$$\  $$ |$$  __$$\ ")
+print ("\033[1;96m $$ |  $$ |$$ /  $$ |$$ |  $$ |$$$$\ $$ |$$ /  $$ |")
+print ("\033[1;96m $$$$$$$  |$$ |  $$ |$$$$$$$  |$$ $$\$$ |$$ |  $$ |")
+print ("\033[1;97m $$  ____/ $$ |  $$ |$$  __$$< $$ \$$$$ |$$ |  $$ |")
+print ("\033[1;97m $$ |      $$ |  $$ |$$ |  $$ |$$ |\$$$ |$$ |  $$ |")
+print ("\033[1;97m $$ |       $$$$$$  |$$ |  $$ |$$ | \$$ | $$$$$$  |")
+print ("\033[1;97m \__|       \______/ \__|  \__|\__|  \__| \______/")
+print ("\033[1;96m──────────────────────────────────────────────────")
+print ("\033[1;97m[\033[1;96m✰\033[1;97m] AUTHOR\033[1;97m: \033[1;97m Vindra")
+print ("\033[1;97m[\033[1;96m✰\033[1;91m] WARNING\033[1;97m: \033[1;97m setiap senin dan minggu update jam 00:00 malam")
+print ("\033[1;97m[\033[1;96m✰\033[1;91m] WARNING\033[1;97m: \033[1;97mJANGAN DI AMBIL SCNYA KARNA AD UPDATEAN TERBARU")
+print ("\033[1;96m──────────────────────────────────────────────────")
+
+
+os.system('xdg-open https://wa.me/+6283809192307?text=Assalamualaikum+Bang+Mau+Beli+Key+,+Harga+Key+Berapa+Ya+Bang?')
+time.sleep(2.5)
+input("\n\x1b[1;93mKLIK ENTER UNTUK MELANJUTKAN ")
+time.sleep(1.5)
+loop = 'true'
+while (loop == 'true'):
+    cabulnyai = input("\x1b[1;92mMASUKAN KEY UNTUK LOGIN:\x1b[1;93m")
+    if (cabulnyai == vindragamingslewebjancokmenabnjaylelebwelehphqvindraanjaygialaselebejeksie):
+            loop = 'false'
+            print("\x1b[1;92mLOADING KEY... ")
+            time.sleep(4.8)
+            print("\x1b[1;92mKEY YANG ANDA MASUKKAN BENAR")
+            time.sleep(3.9)
+    else:
+               exit("\x1b[1;91mPASSWORD SALAH (X) CHAT NOMOR 083809192307")
+
+os.system('clear')
+time.sleep(1.9)
+print ("\033[1;96m $$$$$$$\   $$$$$$\  $$$$$$$\  $$\   $$\  $$$$$$\  ")
+print ("\033[1;96m $$  __$$\ $$  __$$\ $$  __$$\ $$$\  $$ |$$  __$$\ ")
+print ("\033[1;96m $$ |  $$ |$$ /  $$ |$$ |  $$ |$$$$\ $$ |$$ /  $$ |")
+print ("\033[1;96m $$$$$$$  |$$ |  $$ |$$$$$$$  |$$ $$\$$ |$$ |  $$ |")
+print ("\033[1;97m $$  ____/ $$ |  $$ |$$  __$$< $$ \$$$$ |$$ |  $$ |")
+print ("\033[1;97m $$ |      $$ |  $$ |$$ |  $$ |$$ |\$$$ |$$ |  $$ |")
+print ("\033[1;97m $$ |       $$$$$$  |$$ |  $$ |$$ | \$$ | $$$$$$  |")
+print ("\033[1;97m \__|       \______/ \__|  \__|\__|  \__| \______/")
+                                                  
+						 
+print ("\033[1;96m──────────────────────────────────────────────────")
+print ("\033[1;97m[\033[1;96m✰\033[1;97m] AUTHOR\033[1;97m: \033[1;97m Vindra")
+print ("\033[1;97m[\033[1;96m✰\033[1;97m] Follow bg\033[1;97m: \033[1;97mhttps://www.facebook.com/nano.giung ")
+print ("\033[1;97m[\033[1;96m✰\033[1;91m] WARNING\033[1;97m: \033[1;97mJANGAN DI AMBIL SCNYA KARNA ADA UPDATEAN TERBARU")
+print ("\033[1;96m──────────────────────────────────────────────────")
+print ("\033[1;97m{\033[1;96m01\033[1;97m} haus ngeue  40 vid  \033[1;97m{\033[1;96m06\033[1;97m} Prank Ojol10 vid  \033[1;97m{\033[1;96m11\033[1;97m} BOKEP")
+print ("\033[1;97m{\033[1;96m02\033[1;97m} idaman bgt  38 vid  \033[1;97m{\033[1;96m07\033[1;97m} Indo ikat 21 vid  \033[1;97m{\033[1;96m12\033[1;97m} Follow fbku")
+print ("\033[1;97m{\033[1;96m03\033[1;97m} TanteGirang  8 vid  \033[1;97m{\033[1;96m08\033[1;97m} Pramuka ngeue")
+print ("\033[1;97m{\033[1;96m04\033[1;97m} MaenDiKebun 19 vid  \033[1;97m{\033[1;96m09\033[1;97m} Open Michat 10 vid")
+print ("\033[1;97m{\033[1;96m05\033[1;97m} cindo c0m3k 14 vid  \033[1;97m{\033[1;96m10\033[1;97m} Tube V-Sex")
+print ("\033[1;96m──────────────────────────────────────────────────")
+
+def login():
+	vindra = input("\033[1;97mPILIH NOMOR :")
+	if vindra =="":
+		login()
+		print ("\033[1;97m[\033[1;96m!\033[1;97m] SANGEAN LU !")
+	elif vindra =="1" or vindra =="01":
+		os.system('xdg-open  https://dood.yt/f/fkm1pt5lkc')
+		login()
+	elif vindra =="2" or vindra =="02":
+		os.system('xdg-open  https://dood.yt/f/5kb7jzi3h9')
+		login()
+	elif vindra =="3" or vindra =="03":
+		os.system('xdg-open  https://dood.yt/f/1r5nbu8lmo')
+		login()
+	elif vindra =="4" or vindra =="04":
+		os.system('xdg-open  https://dood.yt/f/xzeehh8bm3')
+		login()
+	elif vindra =="5" or vindra =="05":
+		os.system('xdg-open  https://dood.yt/f/44jekg7q0v')
+		login()
+	elif vindra =="6" or vindra =="06":
+		os.system('xdg-open  https://dood.yt/f/dy6x2o9a0p')
+		login()
+	elif vindra =="7" or vindra =="07":
+		os.system('xdg-open  https://dood.yt/f/k0ahheoiiy')
+		login()
+	elif vindra =="8" or vindra =="08":
+		os.system('xdg-open  https://dood.re/d/gcdh3fd4d46z')
+		login()
+	elif vindra =="9" or vindra =="09":
+		os.system('xdg-open  https://dood.yt/f/7rtp4rdg6l')
+		login()
+	elif vindra =="10" or vindra =="10":
+		os.system('xdg-open  https://www.tubev.sex')
+		login()
+	elif vindra =="11" or vindra =="11":
+                os.system('xdg-open  https://xnxx-com.xxx/')
+                login()
+	elif vindra =="12" or vindra =="12":
+                os.system('xdg-open  https://www.facebook.com/nano.giung')
+
+	else:
+		print ("\033[1;97m[\033[1;96m!\033[1;97m] CABUL PILIH YANG BENER")
+	
+		
+if __name__=='__main__':
+	login()
